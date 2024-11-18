7f3e11f69a24bd1dd65da65129f4a8227a82eeaf
MohammadYehya <mohammad7446@gmail.com>
2024-11-18 22:58:15 +0500

    Refactor Code & updated test images

--- main.py
+++ main.py
@@ -1,92 +1,10 @@
-from qiskit import IBMQ, QuantumCircuit, ClassicalRegister, QuantumRegister
-from qiskit import execute, QuantumRegister
-from qiskit.qasm import pi
-from qiskit.tools.visualization import plot_histogram, circuit_drawer
-from qiskit import execute, Aer, BasicAer
-from PIL import Image
-import numpy as np
-# import random
-# import keras
-# from keras.models import Sequential
-# from keras.layers import Dense, Activation
-# from keras.datasets import mnist
-import matplotlib.pyplot as plt
-# from sklearn.metrics import mean_squared_error, mean_absolute_error, mutual_info_score, r2_score
-# from qiskit.circuit.library.standard_gates import RYGate, RYYGate
-# import cv2
-import pandas as pd
-from utils import *
+from quantumimageencoding import *
+from utils import showdiff
 
-# Read the csv file
-# dataset = pd.read_csv('mnist-resized.csv')
-# Reshape and transform the dataframe into a numpy array
-# images = dataset.to_numpy()[:,1:].reshape(42000,8,8)
-# pixel_values = images.reshape(42000,64)
+FRQIEncoder = FRQI()
+image1 = FRQIEncoder.preProcessImage(Image.open('./testimages/test8.png').convert('L'))
+FRQIEncoder.encode(image1)
+image2 = FRQIEncoder.decode('aer_simulator', shots=2**24)
 
+showdiff(FRQIEncoder, image1, image2)
 
-numQubits = 9
-
-
-images = Image.open('./test16.png')
-images = image_preparation(images)
-# images = make_square_image(images)
-fig = plt.figure()
-fig.add_subplot(2,1,1)
-plt.imshow(images, cmap='gray')
-
-qc = make_circ(images, numQubits)
-# qc.draw(output = 'mpl')
-counts = get_counts(qc, numQubits)
-retrieve_image = decoding(counts, numQubits)
-
-
-
-
-# normalized_pixels = pixel_values/255.0
-# angles = np.arcsin(normalized_pixels[0,:])
-
-# qr = QuantumRegister(7,'q')
-# cr = ClassicalRegister(7,'c')
-# qc = QuantumCircuit(qr,cr)
-
-# # Call the frqi function and add all the necessary parameters
-# frqi(qc, [0,1,2,3,4,5], 6, angles)
-# qc.draw(output = 'mpl')
-
-# qc.measure([0,1,2,3,4,5,6],[0,1,2,3,4,5,6])
-
-# backend_sim = Aer.get_backend('qasm_simulator')
-# numOfShots = 1048576
-# result = execute(qc, backend_sim, shots=numOfShots).result()
-
-# print(result.get_counts(qc))
-
-# plot_histogram(result.get_counts(qc), figsize=(20,7))
-
-
-
-
-# retrieve_image = np.array([])
-
-
-
-
-
-# for i in range(64):
-#   try:
-#     s = format(i, '06b')
-#     new_s = '1' + s
-#     retrieve_image = np.append(retrieve_image,np.sqrt(result.get_counts(qc)[new_s]/numOfShots))
-#   except KeyError:
-#     retrieve_image = np.append(retrieve_image,[0.0])
-
-# retrieve_image *=  8.0*255.0
-# retrieve_image = retrieve_image.astype('int')
-# retrieve_image = retrieve_image.reshape((8,8))
-
-
-
-
-fig.add_subplot(2,1,2)
-plt.imshow(retrieve_image, cmap='gray', vmin=0, vmax=255)
-plt.show()
