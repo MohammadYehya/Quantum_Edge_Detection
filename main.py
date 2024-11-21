b3dd10fe5e784400130caf29013e3dfbef0b21b5
MohammadYehya <mohammad7446@gmail.com>
2024-11-22 00:27:22 +0500

    bug encountered in executing qiskit circuits

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from matplotlib import pyplot as plt
 from quantumimageencoding.FRQI import FRQI
 from quantumimageencoding.QPIE import QPIE
 from utils import showdiff
 from PIL import Image
 
 Encoder = QPIE()
-image1 = Encoder.preProcessImage(Image.open('./testimages/test4edges.png').convert('L'))
+#try binary image
+# image1 = Encoder.preProcessImage(Image.open('./testimages/test4edges.png').convert('L'))
+arr = [ [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]]
+
+image1 = arr
 Encoder.encode(image1)
 image2 = Encoder.detectEdges()
 showdiff(Encoder, image1, image2)
--- quantumimageencoding/QPIE.py
+++ quantumimageencoding/QPIE.py
@@ -1,4 +1,4 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import QuantumCircuit
 from qiskit.circuit.library.standard_gates import RYGate
 from quantumimageencoding.BaseQuantumEncoder import QuantumEncoder
 from qiskit import execute, Aer
@@ -13,8 +13,8 @@ class QPIE(QuantumEncoder):
         img = img.astype(numpy.float64)
         rms = numpy.sqrt(numpy.sum(numpy.sum(img**2, axis=1)))
         amplitudes = (img/rms).reshape(img.shape[0]**2)
-        print(amplitudes)
-        print(numpy.sum(amplitudes**2))
+        # print(amplitudes)
+        # print(numpy.sum(amplitudes**2))
         return amplitudes
     
     def encode(self, image : Image.Image) -> None :
@@ -26,31 +26,37 @@ class QPIE(QuantumEncoder):
         unitaryMatrix = numpy.identity(2**(controlbits+1))
         unitaryMatrix = numpy.roll(unitaryMatrix,1,axis=1)
 
-        positions = QuantumRegister(controlbits, 'position')
-        target = QuantumRegister(1, 'target')
-        classical = ClassicalRegister(controlbits+1, 'measure')
+        # positions = QuantumRegister(controlbits, 'position')
+        # target = QuantumRegister(1, 'target')
+        # classical = ClassicalRegister(controlbits+1, 'measure')
 
-        self.Qcirc = self.createQuantumCircuit(positions, target, classical)
+        # self.Qcirc = self.createQuantumCircuit(positions, target, classical)
+        self.Qcirc = QuantumCircuit(controlbits+1)
         self.Qcirc.initialize(h_amplitudes, range(1, controlbits+1))
         self.Qcirc.h(0)
         self.Qcirc.unitary(unitaryMatrix, range(controlbits+1))
         self.Qcirc.h(0)
         
-        self.Qcirc2 = self.createQuantumCircuit(positions, target, classical)
+        # self.Qcirc2 = self.createQuantumCircuit(positions, target, classical)
+        self.Qcirc2 = QuantumCircuit(controlbits+1)
         self.Qcirc2.initialize(v_amplitudes, range(1, controlbits+1))
         self.Qcirc2.h(0)
         self.Qcirc2.unitary(unitaryMatrix, range(controlbits+1))
         self.Qcirc2.h(0)
 
+        # self.Qcirc.draw('mpl', fold=-1)
+        # self.Qcirc2.draw('mpl', fold=-1)
 
     def decode(self, simulator : str, shots: int = 2**16) -> Image.Image :
         pass
 
     def detectEdges(self) -> Image.Image:
-        backend_sim = Aer.get_backend('statevector_simulator')
-        results = execute([self.Qcirc, self.Qcirc2], backend=backend_sim).result()
+        back = Aer.get_backend('statevector_simulator')
+        # self.Qcirc2.draw('mpl', fold=-1)
+        results = execute([self.Qcirc, self.Qcirc2], backend=back).result()
         state_vector_h = results.get_statevector(self.Qcirc)
         state_vector_v = results.get_statevector(self.Qcirc2)
+
 
         size = int(2**((self.Qcirc.num_qubits-1)/2))
         threshold = lambda amp: (amp > 1e-15 or amp < -1e-15)
@@ -58,6 +64,7 @@ class QPIE(QuantumEncoder):
         v_edge_scan_img = numpy.abs(numpy.array([1 if threshold(state_vector_v[(2*i)+1].real) else 0 for i in range(2**(self.Qcirc2.num_qubits-1))])).reshape(size, size).T
 
         edge_scan_image = h_edge_scan_img | v_edge_scan_img
-        print(edge_scan_image)
+        # print(h_edge_scan_img, v_edge_scan_img)
+        # print(edge_scan_image)
         # return Image.fromarray(edge_scan_image.astype(int))
         return edge_scan_image
